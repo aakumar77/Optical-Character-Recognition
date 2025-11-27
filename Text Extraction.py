254d2fddaae21e4c991b3d3cf22ba54bcc9708d7
Aayush Kumar <aayush.kumar2422@gmail.com>
2025-11-27 21:37:42 +0530

    Manipulating Images using OpenCV in Jupyter

--- Text Extraction.py
+++ Text Extraction.py
@@ -1,3 +1,14 @@
-from PIL import image
-import cv2
-import pytesseract
+from PIL import Image
+#import cv2
+#import pytesseract
+
+
+img_file='data.jpg'
+
+im=Image.open(img_file)
+#print(im)
+#print(im.size)
+#im.show()
+#im.rotate(90).show()
+
+im.save("Images/data.jpg")
